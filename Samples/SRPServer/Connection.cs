0d1e1f22f77891e76067499c106bdb70e5ce0466
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-12-03 13:17:23 +0100

    Added Sample Listener

---
+++ Samples/SRPServer/Connection.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+using Lidgren.Network.Authentication;
+using System.Timers;
+
+namespace SRPServer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Connection : IDisposable
+    {
+        private Boolean _connected, _disposed;
+        private INetEncryption _netEncryption;
+        private Timer _keepAliveTimer;
+
+        /// <summary>
+        /// Lidgren NetConnection (Pipe)
+        /// </summary>
+        public NetConnection NetConnection { get; protected set; }
+
+        /// <summary>
+        /// Local Lidgrend NetPeer (Origin)
+        /// </summary>
+        public NetPeer NetManager { get; protected set; }
+
+        /// <summary>
+        /// Node id
+        /// </summary>
+        public String NodeId { get; set; }
+
+        /// <summary>
+        /// Node username
+        /// </summary>
+        public String Username { get; set; }
+
+        /// <summary>
+        /// Connected flag
+        /// </summary>
+        public Boolean IsConnected
+        {
+            get { return _connected; }
+        }
+
+        /// <summary>
+        /// Disposed flag
+        /// </summary>
+        public Boolean IsDisposed
+        {
+            get { return _disposed; }
+        }
+
+        /// <summary>
+        /// Transfer flag
+        /// </summary>
+        public Boolean IsTransfering
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="manager">Local NetPeer</param>
+        /// <param name="con">NetConnection</param>
+        /// <param name="key">Encryption key</param>
+        /// <param name="nodeId">Node Id</param>
+        public Connection(NetPeer manager, NetConnection con, INetEncryption encryption, String nodeId)
+        {
+            // This keep alive timer is used to overcome BAD connections that drop ping/pong messages a lot.
+            // It simply sends an empty message every five seconds. As long as some messages are retained
+            // and received on the connection, all will be well
+            _keepAliveTimer = new System.Timers.Timer(5000);
+            _keepAliveTimer.Elapsed += new ElapsedEventHandler((Object state, ElapsedEventArgs args) => 
+                this.SendMessage(this.NetManager.CreateMessage(0), NetDeliveryMethod.Unreliable));
+
+            this.Username = (con.Tag as Handshake).Username;
+            this.NetManager = manager;
+            this.NetConnection = con;
+            this.NetConnection.Tag = this;
+            this.NodeId = nodeId;
+
+            if (encryption != null)
+            {
+                _netEncryption = encryption;
+            }
+            else
+            {
+                // You could write code that makes it possible to transfer users between server
+                // where you don't have any encryption key YET. The place to start that would
+                // be here.
+                this.IsTransfering = true;
+                _netEncryption = new NetXtea(new Byte[16]);
+            }
+
+            // Not connected until everything is done.
+            System.Threading.Thread.MemoryBarrier();
+
+            _connected = true;
+            _keepAliveTimer.Start();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="manager">Local NetPeer</param>
+        /// <param name="con">NetConnection</param>
+        /// <param name="key">Node Id</param>
+        public Connection(NetPeer manager, NetConnection con, Byte[] key)
+            : this(manager, con, key != null ? new NetXtea(key) : null, String.Empty)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="con"></param>
+        /// <param name="encryption"></param>
+        public Connection(NetPeer manager, NetConnection con, INetEncryption encryption)
+            : this(manager, con, encryption, String.Empty)
+        {
+
+        }
+
+        /// <summary>
+        /// Transfer constructor
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="con"></param>
+        public Connection(NetPeer manager, NetConnection con, String id)
+            : this(manager, con, null, id) { }
+
+        /// <summary>
+        /// This handles an incoming message 
+        /// </summary>
+        /// <param name="msg">The received message</param>
+        public void IncomingMessage(NetIncomingMessage msg)
+        {
+            if (!this.IsConnected)
+                return;
+
+            // Is this is a heartbeat message? (keep alive)
+            if (msg.LengthBits == 0)
+                return;
+
+            // Decrypt the message
+            msg.Decrypt(_netEncryption);
+            if (msg.LengthBits == 0)
+                return;
+
+            // Here you can process your message like:
+            Console.WriteLine("Received message: {0}", msg.ReadString());
+        }
+
+        /// <summary>
+        /// Sends a message
+        /// </summary>
+        /// <param name="msg">The message to send</param>
+        /// <param name="method">The delivery method</param>
+        /// <param name="sequenceChannel">The sequence channel</param>
+        public void SendMessage(NetOutgoingMessage msg, NetDeliveryMethod method, Int32 sequenceChannel)
+        {
+            msg.Encrypt(_netEncryption);
+            NetConnection.SendMessage(msg, method, sequenceChannel);
+        }
+
+        /// <summary>
+        /// Sends a message
+        /// </summary>
+        /// <param name="msg">The message to send</param>
+        /// <param name="method">The delivery method</param>
+        public void SendMessage(NetOutgoingMessage msg, NetDeliveryMethod method)
+        {
+            SendMessage(msg, method, 0);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or 
+        /// resetting unmanaged resources. Will deregister all protocols
+        /// </summary>
+        public void Dispose()
+        {
+            if(this.IsDisposed)
+                return;
+
+            _keepAliveTimer.Stop();
+            _disposed = true;
+            _keepAliveTimer.Dispose();
+        }
+
+        /// <summary>
+        /// Sets the encryption for this connection (see the comments about transfering clients)
+        /// </summary>
+        /// <param name="key"></param>
+        internal void SetEncryption(INetEncryption enc)
+        {
+            _netEncryption = enc;
+        }
+    }
+}
---
+++ Samples/SRPServer/Listener.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Lidgren.Network;
+using Lidgren.Network.Lobby;
+using Lidgren.Network.Authentication;
+
+namespace SRPServer
+{
+    /// <summary>
+    /// interfaces client <-> gameserver connections
+    /// </summary>
+    internal class Listener
+    {
+        /// <summary>
+        /// Listener running on port
+        /// </summary>
+        public static Int32 Port { get; private set; }
+
+        /// <summary>
+        /// Handles the running switch
+        /// </summary>
+        public Boolean IsRunning { get; set; }
+        
+        /// <summary>
+        /// Gets the net server
+        /// </summary>
+        public NetServer Server { get { return _server; } }
+
+        /// <summary>
+        /// Delegate used when a connection is established or broken
+        /// </summary>
+        /// <param name="nodeId"></param>
+        /// <param name="username"></param>
+        public delegate void ConnectionDelegate(String nodeId, String username);
+
+        /// <summary>
+        /// List of functions to call on connection
+        /// </summary>
+        public event ConnectionDelegate OnConnected = delegate { };
+
+        /// <summary>
+        /// List of functions to call on disconnect
+        /// </summary>
+        public event ConnectionDelegate OnDisconnected = delegate { };
+
+        protected Thread _serverThread;
+        protected NetServer _server;
+
+        /// <summary>
+        /// Client Listener
+        /// </summary>
+        public Listener()
+        {
+            // Configuration
+            NetPeerConfiguration config = new NetPeerConfiguration("Lidgren.Authentication.Sample");
+            config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+            config.Port = 15812;
+            config.ConnectionTimeout = 60 * 15;
+            config.UseMessageRecycling = true;
+
+            _server = new NetServer(config);
+            _server.Start();
+
+            Listener.Port = config.Port;
+        }
+
+        /// <summary>
+        /// Start running thread
+        /// </summary>
+        public void Start()
+        {
+            _serverThread = new Thread(new ThreadStart(ClientLoop));
+            _serverThread.Name = "Client thread";
+
+            this.IsRunning = true;
+
+            _serverThread.Start();
+        }
+
+        /// <summary>
+        /// Client message loop
+        /// </summary>
+        private void ClientLoop()
+        {
+            NetIncomingMessage msg;
+            
+            while (this.IsRunning)
+            {
+                _server.MessageReceivedEvent.WaitOne(1000);
+                msg = _server.ReadMessage();
+
+                // If this second no messages accepted, releave CPU
+                if (msg == null)
+                    continue;
+
+                try
+                {
+                    switch (msg.MessageType)
+                    {
+                        // MESSAGETYPE: DATA
+                        // The main message type in networking is the data type. When the connection is not linked, the
+                        // data is the verification data of the handshake and will be processed accordingly. If not, 
+                        // the message is passed onto the Connection and processed by their respective protocol.
+                        case NetIncomingMessageType.Data:
+
+                            var connection = msg.SenderConnection.Tag as Connection;
+                            if (connection != null)
+                            {
+                                // Client was authenticated so just process the message
+                                connection.IncomingMessage(msg);
+                            }
+                            else
+                            {
+                                // Client is not yet authenticate so handle that. In my case we directly expect
+                                // authentication, but you can write your own handling code. The following code
+                                // handles just that.
+                                var handshake = NetLobby.IncomingMessage(msg);
+
+                                switch (handshake)
+                                {
+                                    case Handshake.Contents.Succes:
+                                        Connection new_connection = new Connection(_server, msg.SenderConnection, (msg.SenderConnection.Tag as Handshake).CreateEncryption());
+                                        OnConnected.Invoke(new_connection.NodeId, new_connection.Username);
+                                        break;
+
+                                    case Handshake.Contents.Error:
+                                    case Handshake.Contents.Denied:
+                                        msg.SenderConnection.Disconnect("Error occured during handshake.");
+                                        break;
+
+                                    case Handshake.Contents.Expired:
+                                        msg.SenderConnection.Disconnect("Handshake expired.");
+                                        break;
+                                }
+                            }
+
+                            break;
+
+                        // MESSAGETYPE: CONNECTION APPROVAL
+                        // The ConnectionApproval message type is seen when a node yields the peer#connect function. When
+                        // the RemoteEndpoint specified is reached, a loose connection is made. It's up to the other end,
+                        // the one that is connected too, to deny or approve the connection. 
+                        case NetIncomingMessageType.ConnectionApproval:
+
+                            // Here you can add approval code to the public section of the server
+                           
+                            msg.SenderConnection.Approve();
+                        
+                            break;
+
+                        // MESSAGETYPE: STATUS CHANGED
+                        // Internal type that is triggered when a connection is initiated, responded too, connecting,
+                        // disconnecting, connected or disconnected. Upon a connection, we might have received some
+                        // RemoteHailData. This is part of the SRPP protocol and is proccesed accordingly. When
+                        // disconnecting, the Connection is disposed, internal connection is disconnected and all is
+                        // logged. 
+                        case NetIncomingMessageType.StatusChanged:
+
+                            NetConnectionStatus statusByte = (NetConnectionStatus)msg.ReadByte();
+                           
+                            switch (statusByte)
+                            {
+                                case NetConnectionStatus.Disconnecting:
+                                    break;
+
+                                case NetConnectionStatus.Disconnected:
+                                    // If already connection established, destroy resources
+                                    var disconnected_connection = msg.SenderConnection.Tag as Connection;
+                                    if (disconnected_connection != null)
+                                    {
+                                        OnDisconnected.Invoke(disconnected_connection.NodeId, disconnected_connection.Username);
+
+                                        if (!disconnected_connection.IsDisposed)
+                                            disconnected_connection.Dispose();
+                                    }
+
+                                    String finalReason = Encoding.UTF8.GetString(msg.ReadBytes((Int32)msg.ReadVariableUInt32()));
+                                    // Do something with the message if you want like logging: Received a reason for disconnecting...
+                                    break;
+
+                                case NetConnectionStatus.Connected:
+                                    break;
+
+                                default:
+                                    String statusChange = Encoding.UTF8.GetString(msg.ReadBytes((Int32)msg.ReadVariableUInt32()));
+                                    // You can log/debug status messages here
+                                    break;
+                            }
+
+                            break;
+
+                            #if DEBUG
+                            case NetIncomingMessageType.DebugMessage:
+                                String debugMessage = Encoding.UTF8.GetString(msg.ReadBytes((Int32)msg.ReadVariableUInt32()));
+                                // log
+                                break;
+                            #endif
+
+                            case NetIncomingMessageType.WarningMessage:
+                                String warningMessage = Encoding.UTF8.GetString(msg.ReadBytes((Int32)msg.ReadVariableUInt32()));
+                                // log;
+                                break;
+
+                            case NetIncomingMessageType.ErrorMessage:
+                                String errorMessage = Encoding.UTF8.GetString(msg.ReadBytes((Int32)msg.ReadVariableUInt32()));
+                                // log
+                                break;
+
+                            default:
+                                throw new NetException("MessageType: " + msg.MessageType + " is not supported.");
+                        }
+
+                    // Recycle please
+                    _server.Recycle(msg);
+                }
+                catch(Exception e) 
+                {
+                    try
+                    {
+                        // Disconnect client on error
+                        msg.SenderConnection.Disconnect("No tolerance: exception " + e.Message);
+                    }
+                    catch (Exception) { }
+                }
+            }
+            
+            // Early shutdown
+            _server.Shutdown("Server shutting down");
+
+            Console.WriteLine("Client Service is Stopping at " + NetTime.ToReadable(NetTime.Now));
+            _server.Shutdown("Final shutdown");
+        }
+    }
+}

ef55dfb3be30e16dbfe6fc9b90f13de2498e7c0f
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-12-03 13:16:51 +0100

    Added flatfile database

---
+++ Samples/SRPServer/PlayerDatabase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SRPServer
+{
+    internal static class PlayerDatabase
+    {
+        private static List<PlayerDatabaseEntry> _entries;
+
+        /// <summary>
+        /// Finds a player in the database
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public static PlayerDatabaseEntry Find(String username)
+        {
+            if (_entries == null)
+                Load();
+
+            return _entries.FirstOrDefault(entry => entry.Username.ToLower() == username.ToLower());
+        }
+
+        /// <summary>
+        /// Loads entries from file
+        /// </summary>
+        public static void Load()
+        {
+            _entries = new List<PlayerDatabaseEntry>();
+            using(BinaryReader fs = new BinaryReader(File.OpenRead("flatdb"))) {
+                var count = fs.ReadInt32();
+                for (; count > 0; count--)
+                    _entries.Add(new PlayerDatabaseEntry()
+                    {
+                        Username = fs.ReadString(),
+                        Verifier = fs.ReadBytes(128),
+                        Salt = fs.ReadBytes(10),
+                        IsBanned = fs.ReadBoolean()
+                    });
+            }
+        }
+
+        /// <summary>
+        /// Saves the database
+        /// </summary>
+        public static void Save()
+        {
+            using (BinaryWriter fs = new BinaryWriter(File.OpenWrite("flatdb")))
+            {
+                fs.Write(_entries.Count);
+                foreach (var entry in _entries)
+                {
+                    fs.Write(entry.Username);
+                    fs.Write(entry.Verifier);
+                    fs.Write(entry.Salt);
+                    fs.Write(entry.IsBanned);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds an entry
+        /// </summary>
+        /// <param name="pde"></param>
+        public static void Add(PlayerDatabaseEntry pde)
+        {
+            _entries.Add(pde);
+        }
+    }
+}
---
+++ Samples/SRPServer/PlayerDatabaseEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network.Authentication;
+using Lidgren.Network;
+
+namespace SRPServer
+{
+    [Serializable]
+    internal class PlayerDatabaseEntry
+    {
+        /// <summary>
+        /// Username for this user
+        /// </summary>
+        public String Username { get; set; }
+
+        /// <summary>
+        /// Verifier bytes
+        /// </summary>
+        public Byte[] Verifier { get; set; }
+
+        /// <summary>
+        /// Salt bytes
+        /// </summary>
+        public Byte[] Salt { get; set; }
+
+        /// <summary>
+        /// Current user is banned
+        /// </summary>
+        public Boolean IsBanned { get; set; }
+
+        /// <summary>
+        /// This functions generates a database entry for the given username and password. It
+        /// is key that this function can only be called from a safe environment and that the 
+        /// password is sent safely to this function. Think about SSL encryption or one time
+        /// one way keys and so forth. Do your best! We actually generate these on the client
+        /// so the password is NEVER sent over the network, and then sent it over an encrypted
+        /// channel. 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="keysize"></param>
+        /// <returns></returns>
+        public PlayerDatabaseEntry Generate(String username, String password, Int32 keysize)
+        {
+            Byte[] salt;
+
+            // Calculates the verifier with a random salt
+            // And we make sure the username is no longer case sensitive
+            NetBigInteger verifier = Handshake.PasswordVerifier(username.ToLower().Trim(), password, keysize, out salt);
+
+            // Returns the new entry
+            return new PlayerDatabaseEntry() {
+                Username = username, 
+                Salt = salt, 
+                Verifier = verifier.ToByteArray() 
+            };
+        }
+    }
+}
